f9e998df1dc951c06b380b72ead4d3e5bbd46341
Kyle Rush <kylerrush@gmail.com>
2015-09-12 11:56:38 -0400

    Upgrade Node, Metalsmith.

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,41 @@
-var collections  = require('metalsmith-collections'),
-    connect      = require('gulp-connect'),
-    cssmixins    = require('postcss-mixins')(),
-    cssnested    = require('postcss-nested')(),
-    cssnext      = require('cssnext')(),
-    del          = require('del'),
-    gulp         = require('gulp'),
-    gulpsmith    = require('gulpsmith'),
-    layouts      = require('metalsmith-layouts'),
-    markdown     = require('metalsmith-markdown'),
-    metalsmith   = require('metalsmith'),
-    moment       = require('moment'),
-    paths        = require('metalsmith-paths'),
-    permalinks   = require('metalsmith-permalinks'),
-    postcss      = require('gulp-postcss'),
-    swig         = require('swig'),
-    templates    = require('metalsmith-templates');
+const collections  = require('metalsmith-collections');
+const connect      = require('gulp-connect');
+const cssmixins    = require('postcss-mixins')();
+const cssnested    = require('postcss-nested')();
+const cssnext      = require('cssnext')();
+const del          = require('del');
+const gulp         = require('gulp');
+const gulpsmith    = require('gulpsmith');
+const layouts      = require('metalsmith-layouts');
+const markdown     = require('metalsmith-markdown');
+const metalsmith   = require('metalsmith');
+const moment       = require('moment');
+const paths        = require('metalsmith-paths');
+const permalinks   = require('metalsmith-permalinks');
+const postcss      = require('gulp-postcss');
+const swig         = require('swig');
+const templates    = require('metalsmith-templates');
 
 
-gulp.task('del', function(){
+gulp.task('del', () => {
   del('./build');
 });
 
-gulp.task('connect', function(){
+gulp.task('connect', () => {
   connect.server({
     root: './build',
     livereload: true
   });
 });
 
-gulp.task('html', function(){
+gulp.task('connect', () => {
+  connect.server({
+    root: './build',
+    livereload: true
+  });
+});
+
+gulp.task('html', () => {
 
   gulp.src('./src/html/content/**/*.{html,md}')
     .pipe(
@@ -55,16 +62,20 @@ gulp.task('html', function(){
     .pipe(connect.reload());
 });
 
-gulp.task('css', function(){
+gulp.task('css', () => {
   gulp.src('./src/css/styles.css')
     .pipe(postcss([cssnext, cssnested, cssmixins]))
     .pipe(gulp.dest('./build/css'))
     .pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', () => {
   gulp.watch('./src/css/**/*.css', ['css']);
   gulp.watch('./src/html/**', ['html']);
 });
 
 gulp.task('dev', ['connect', 'html', 'css', 'watch']);
+
+gulp.task('default', () => {
+  console.log('hi');
+});
